74544d7ec61c46306e7a61125c3005f3b8a5cac2
Kattni Rembor <kattni@adafruit.com>
2018-07-03 14:03:20 -0400

    sound meter code updated to cpx lib

--- cpx_sound_meter.py
+++ cpx_sound_meter.py
@@ -1,15 +1,8 @@
 import array
+import math
 import audiobusio
 import board
-import math
-import neopixel
-
-CURVE = 2
-SCALE_EXPONENT = math.pow(10, CURVE * -0.1)
-
-PEAK_COLOR = (80, 0, 255)
-NUM_PIXELS = 10
-NUM_SAMPLES = 160
+from adafruit_circuitplayground.express import cpx
 
 
 def constrain(value, floor, ceiling):
@@ -18,28 +11,17 @@ def constrain(value, floor, ceiling):
 
 def log_scale(input_value, input_min, input_max, output_min, output_max):
     normalized_input_value = (input_value - input_min) / (input_max - input_min)
-    return output_min + math.pow(normalized_input_value, SCALE_EXPONENT) * (output_max - output_min)
+    return output_min + math.pow(normalized_input_value, 0.630957) * (output_max - output_min)
 
 
 def normalized_rms(values):
-    minbuf = int(mean(values))
-    return math.sqrt(sum(float(sample - minbuf) * (sample - minbuf) for sample in values) / len(values))
+    minbuf = int(sum(values) / len(values))
+    return math.sqrt(sum(float(sample - minbuf) *
+                         (sample - minbuf) for sample in values) / len(values))
 
-
-def mean(values):
-    return sum(values) / len(values)
-
-
-def volume_color(i):
-    return i * (255 // NUM_PIXELS), 50, 0
-
-
-pixels = neopixel.NeoPixel(board.NEOPIXEL, NUM_PIXELS, brightness=0.1, auto_write=False)
-pixels.fill(0)
-pixels.show()
 
 mic = audiobusio.PDMIn(board.MICROPHONE_CLOCK, board.MICROPHONE_DATA, frequency=16000, bit_depth=16)
-samples = array.array('H', [0] * NUM_SAMPLES)
+samples = array.array('H', [0] * 160)
 mic.record(samples, len(samples))
 input_floor = normalized_rms(samples) + 10
 
@@ -51,19 +33,19 @@ peak = 0
 while True:
     mic.record(samples, len(samples))
     magnitude = normalized_rms(samples)
-    print((magnitude),)
+    print((magnitude,))
 
     c = log_scale(constrain(magnitude, input_floor, input_ceiling),
-                  input_floor, input_ceiling, 0, NUM_PIXELS)
+                  input_floor, input_ceiling, 0, 10)
 
-    pixels.fill(0)
-    for i in range(NUM_PIXELS):
+    cpx.pixels.fill((0, 0, 0))
+    for i in range(10):
         if i < c:
-            pixels[i] = volume_color(i)
+            cpx.pixels[i] = (i * (255 // 10), 50, 0)
         if c >= peak:
-            peak = min(c, NUM_PIXELS - 1)
+            peak = min(c, 10 - 1)
         elif peak > 0:
             peak = peak - 1
         if peak > 0:
-            pixels[int(peak)] = PEAK_COLOR
-    pixels.show()
+            cpx.pixels[int(peak)] = (80, 0, 255)
+    cpx.pixels.show()
